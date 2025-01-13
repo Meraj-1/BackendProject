8651fe2acc949c1eac08246c318afb6a60d4113b
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-01-14 01:16:41 +0530

    created utils api errors handler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv"
-
 import connectDB from "./db/index.js";
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000),()=> {
+        console.log(`Connecting to ${process.env.PORT}`);        
+    }
+})
+.catch((err) => {
+    console.log("DATABASE CONNECTION ERROR: " , err);    
+})
 
 
 
