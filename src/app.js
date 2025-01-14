6ac8ee469cca23bf006025b0d0726dd63f7a572d
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-01-14 17:22:00 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
 
-export default {app} ;
+//routes import 
+import userRouter from "./routes/user.route.js"
+
+
+//routes decelaration
+app.use("/api/v1/users", userRouter)
+
+
+export default app ;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import dotenv from "dotenv"
+
+import express from "express";
+import  app  from "./app.js"
 import connectDB from "./db/index.js";
 dotenv.config({
     path: './env'
 })
-
 connectDB()
 .then(()=> {
-    app.listen(process.env.PORT || 8000),()=> {
-        console.log(`Connecting to ${process.env.PORT}`);        
-    }
-})
+    app.listen(process.env.PORT || 8000,()=> {
+        console.log(`server is runing on port ${process.env.PORT}`);        
+     }
+)})
 .catch((err) => {
     console.log("DATABASE CONNECTION ERROR: " , err);    
 })
